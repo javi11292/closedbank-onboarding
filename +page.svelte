e9bf88de706aed5c9cdbf8629be5b45e327741df
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-11 03:36:06 +0100

    Add header

--- +page.svelte
+++ +page.svelte
@@ -1,1 +1,13 @@
-ONBOARDING
+<script lang="ts">
+	import sid1 from "$lib/assets/images/sid1.jpg";
+	import AppBar from "$lib/components/app-bar";
+	import Image from "$lib/components/image";
+</script>
+
+<AppBar>
+	<span class="text-4xl">Closedbank</span>
+</AppBar>
+
+<div class="p-2">
+	<Image src={sid1} />
+</div>
