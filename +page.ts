65aecc56b1e529403001a89c23529deba490de67
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-14 21:47:56 +0100

    Remove default value

--- +page.ts
+++ +page.ts
@@ -3,6 +3,6 @@ import * as Country from "$onboarding/components/country";
 import type { PageLoad } from "./$types";
 
 export const load = (async ({ params }) => {
-	const { language = "es" } = params;
+	const { language } = params;
 	return { imports: { Country: (await loader(Country, language)).default } };
 }) satisfies PageLoad;
